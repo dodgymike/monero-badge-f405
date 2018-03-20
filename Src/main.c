d5e0f0cb2680d1260c7ed94a3ef3c444ea4c28d7
Mike Davis <dodgymike@gmail.com>
2018-03-21 00:15:50 +0200

    Remove extra pin toggling

--- Src/main.c
+++ Src/main.c
@@ -138,11 +138,9 @@ int main(void)
   /* USER CODE BEGIN 3 */
 
         HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
         HAL_Delay(200);
 
         HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
         HAL_Delay(200);
 
   }
