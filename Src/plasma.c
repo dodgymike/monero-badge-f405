d278fed932f12c5ca11f1a60e52019d90d8ea712
Mike Davis <dodgymike@gmail.com>
2018-08-09 18:41:13 +0200

    Plasma working, but no buttons

--- Src/plasma.c
+++ Src/plasma.c
@@ -5,6 +5,17 @@ float dist(float a, float b, float c, float d) {
 }
 
 /*
+int32_t dist(int32_t a, int32_t b, int32_t c, int32_t d) {
+	a *= 1000;
+	b *= 1000;
+	c *= 1000;
+	d *= 1000;
+
+	return (int32_t)(sqrt(((a - c) * (a - c) + (b - d) * (b - d)))) / 1000;
+}
+*/
+
+/*
 void buildSinLookupTable(float sinLookupTable[628]) {
 	for(float r = 0.0f; r <= 6.28f; r += 0.01) {
 		sinLookupTable[(int)(r * 100.0f)] = sin(r);
@@ -30,21 +41,28 @@ void plasma(uint32_t brightness) {
 	timeCounter += 3;
 	float time = timeCounter;
 
-	for(int y = 0; y < screenHeight; y++) {
-		for(int x = 0; x < screenWidth; x++) {
-			float value = sin(dist(x + time, y, 128.0, 128.0) / 8.0)
-				+ sin(dist(x, y, 64.0, 64.0) / 8.0)
-				+ sin(dist(x, y + time / 7, 192.0, 64) / 7.0)
-				+ sin(dist(x, y, 192.0, 100.0) / 8.0);
-			//int colour = (4 + value) * 32;
-			int colour = (4 + value) * 32;
-
-			setPixel(x, y, brightness, colour, colour * 2, 64 - colour);
-
-			uint8_t debugText[10];
- 			sprintf(debugText, "colour (%.5d) value (%.6d)\r\n", colour, ((int)(value * 1000)));
- 			CDC_Transmit_FS(debugText, strlen(debugText));
-
+	if(timeCounter % 3 == 0) {
+		for(int y = 0; y < screenHeight; y++) {
+			for(int x = 0; x < screenWidth; x++) {
+				float value = sin(dist(x + time, y, 128.0, 128.0) / 8.0)
+					+ sin(dist(x, y, 64.0, 64.0) / 8.0)
+					+ sin(dist(x, y + time / 7, 192.0, 64) / 7.0)
+					+ sin(dist(x, y, 192.0, 100.0) / 8.0);
+				int colour = (4 + value) * 32;
+	
+				uint8_t r = colour;
+				uint8_t g = colour * 2;
+				uint8_t b = 64 - colour;
+	
+				setPixel(x, y, brightness, r, g, b);
+	
+				/*
+				uint8_t debugText[100];
+ 				sprintf(debugText, "colour (%.5d) value (%.6d)\r\n", colour, ((int)(value * 1000)));
+ 				CDC_Transmit_FS(debugText, strlen(debugText));
+				*/
+	
+			}
 		}
 	}
 }
